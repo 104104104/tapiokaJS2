a13910e9af319be7d5d3b77879d86e70897e6458
吉田信将 <yoshidatoshinobu@yoshidashinshounoMacBook.local>
2019-07-02 19:56:45 +0900

    一個づつ増えるようにした

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ var ASSETS = {
   },
 };
 
+// マウスの定義
 phina.define("Mouse", {
   superClass: 'CircleShape',
 
@@ -33,8 +34,36 @@ phina.define("Mouse", {
     this.y = p.y;
   },
 });
+// マウスの定義　終わり
 
+// タピオカの定義
+phina.define("Tapioka", {
+  superClass: "Sprite",
 
+  init: function(options) {
+    this.superInit('tapioka');
+    this.x=0;
+    this.y=0;
+    this.width=64;
+    this.height=64;
+    var nowvec=Vector2().addChildTo(this);
+  },
+
+  update: function(app) {
+    var p = app.pointer;
+    if (this.x >= p.x) {
+      this.x -= Math.floor(Math.random() * (1 + 1 - 1)) + 10;
+    } else {
+      this.x += Math.floor(Math.random() * (1 + 1 - 1)) + 10;
+    }
+    if (this.y >= p.y) {
+      this.y -= Math.floor(Math.random() * (1 + 1 - 1)) + 10;
+    } else {
+      this.y += Math.floor(Math.random() * (1 + 1 - 1)) + 10;
+    }
+  },
+});
+//タピオカの定義終わり
 
 phina.define('MainScene', {
   superClass: 'DisplayScene',
@@ -42,36 +71,35 @@ phina.define('MainScene', {
   init: function() {
     this.superInit();
 
-    this.backgroundColor = '#222';
+    this.backgroundColor = '#999';
 
     var mouse = Mouse().addChildTo(this);
     mouse.x = 0;
     mouse.y = 0;
 
+
+    var label = Label({
+      text: '',
+      fontSize: 48,
+      x: this.gridX.center(),
+      y: this.gridY.center(),
+    }).addChildTo(this);
+
     // グループを生成
     this.tapiokas = DisplayElement().addChildTo(this);
 
-    (TAPIOKA_MAX_NUM).times(function(i) {
-        var tapioka = Sprite('tapioka').addChildTo(this.tapiokas);
-        tapioka.x = this.gridX.center();
-        tapioka.y = this.gridY.center();
-        tapioka.width = 64;
-        tapioka.height = 64;
-        tapioka.update = function(app) {
-          var p = app.pointer;
-          if (this.x >= p.x) {
-            this.x -= Math.floor( Math.random() * (100 + 1 - 1) ) + 1 ;
-          } else {
-            this.x += Math.floor( Math.random() * (100 + 1 - 1) ) + 1 ;
-          }
-          if (this.y >= p.y) {
-            this.y -= Math.floor( Math.random() * (100 + 1 - 1) ) + 1 ;
-          } else {
-            this.y += Math.floor( Math.random() * (100 + 1 - 1) ) + 1 ;
-          }
-        }
-      },
-      this);
+    var tapioka = Tapioka().addChildTo(this.tapiokas);
+
+
+    // 更新処理
+    this.update = function(app) {
+      // 経過秒数表示
+      label.text = '経過秒数：' + app.frame +" " +app.frame % 50;
+      if (app.frame % 50 == 0) {
+        var tapioka = Tapioka().addChildTo(this.tapiokas);
+
+      }
+    };
 
   },
 });
